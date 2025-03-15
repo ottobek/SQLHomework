a110467852375a8167573ec433438eabc56f7bba
ottobek <37669297+ottobek@users.noreply.github.com>
2025-03-15 17:51:35 +0500

    Update answers.sql

--- lesson-1/homework/answers.sql
+++ lesson-1/homework/answers.sql
@@ -1,1 +1,13 @@
+-- Introduction to SQL Server and SSMS 
 
+-- EASY
+
+-- 1. Define the following terms: data, database, relational database, and table.
+
+-- Data is a piece of information that in isolation may not carry any meaning (unlike information). It can be digits, text or symbols. 
+-- Database is comprised of data and unlike text that we know it through books, for example, database usually consists of tables. 
+-- Database is comprised of data and unlike text that we know it through books, for example, database (one word) usually consists of tables. 
+-- In a relational database, each table represents a specific entity or concept and they are connected to each other through relationships. For instance, you might have an Employees table, a Departments table, and a Projects table that are linked together.
+
+-- 2. List five key features of SQL Server. 
+-- Query Language - SQL Server uses SQL (Structured Query Language) to let you ask questions about your data.
